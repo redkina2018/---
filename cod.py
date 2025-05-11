21d8397ae21e85c74dde4675de66bbc9dd337ac4
redkina2018 <135330172+redkina2018@users.noreply.github.com>
2025-05-11 14:02:03 +0300

    1.0
    отображение фона

---
+++ cod.py
@@ -0,0 +1,21 @@
+from pygame import *
+
+#игровая сцена:
+back = (200, 255, 255) #цвет фона (background)
+window = display.set_mode((600, 500))
+window.fill(back)
+clock = time.Clock()
+
+#флаги, отвечающие за состояние игры
+game = True
+finish = False
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+ 
+    display.update()
+    clock.tick(60)
+
+
